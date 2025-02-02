d8481feb599e359971f855726c28ac30577566e5
quartztz <quartztz@protonn.me>
2025-02-02 21:27:18 +0100

    feat: luminence-based font color handling

--- src/components/TimeSlot.tsx
+++ src/components/TimeSlot.tsx
@@ -33,14 +33,35 @@ const TimeSlotComponent = (
     style.width = `${100 / slot.conflicts - 2}% `
   }
 
+  const textColor = (bg: string): "text-black" | "text-white" => {
+    const hexToRgb = (hex: string) => {
+      const bigint = parseInt(hex.slice(1), 16);
+      return [(bigint >> 16) & 255, (bigint >> 8) & 255, bigint & 255];
+    }
+
+    const rgb = hexToRgb(bg);
+    if (rgb) {
+      // from: https://en.wikipedia.org/wiki/YCbCr#Y%E2%80%B2CbCr_to_xvYCC
+      // inshallah this works
+      const brightness = Math.round(
+        (rgb[0] * 299 +
+          rgb[1] * 587 +
+          rgb[2] * 114) /
+        1000
+      )
+      return brightness > 125 ? "text-black" : "text-white"
+    }
+    return "text-black"
+  }
+
   return (
     <div
       style={style}
       key={`${course.name} ${slot.day} ${slot.startTime}`}
       className="flex flex-col items-start justify-start absolute rounded p-1">
 
-      <div className="text-white font-semibold leading-5 text-sm">{course.name}</div>
-      <div className="text-white text-xs">{slot.kind}</div>
+      <div className={`${textColor(course.color)} font-semibold leading-5 text-sm`}>{course.name}</div>
+      <div className={`${textColor(course.color)} text-white text-xs`}>{slot.kind}</div>
     </div>
   );
 }
