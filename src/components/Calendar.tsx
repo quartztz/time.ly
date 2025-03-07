88d14a88471c5169b309fc36c3d815ef22fd24b3
quartztz <quartztz@protonn.me>
2025-03-07 11:28:27 +0100

    style: reformat

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,15 +1,11 @@
-import {
-  Card,
-  CardContent,
-  CardHeader
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import * as c from "@/lib/Common";
-import type { Course, CalendarConfig } from '@/lib/Types';
-import { DayComponent } from '@/components/Day';
+import type { Course, CalendarConfig } from "@/lib/Types";
+import { DayComponent } from "@/components/Day";
 
 interface CalendarProps {
-  config: CalendarConfig,
-  courses: Course[]
+  config: CalendarConfig;
+  courses: Course[];
 }
 
 const Calendar = ({ config, courses }: CalendarProps) => {
@@ -23,44 +19,63 @@ const Calendar = ({ config, courses }: CalendarProps) => {
 
   const gridLayouts: { [key: number]: string } = {
     1: "grid-cols-1",
+    2: "grid-cols-2",
     3: "grid-cols-3",
+    4: "grid-cols-4",
     5: "grid-cols-5",
+    6: "grid-cols-6",
     7: "grid-cols-7",
   };
 
   return (
     <div className="w-full h-full flex flex-col">
-      <Card id="calendar" className="w-full h-full flex flex-col justify-center items-center bg-white p-4">
+      <Card
+        id="calendar"
+        className="w-full h-full flex flex-col justify-center items-center bg-white p-4"
+      >
         <CardHeader className="w-full flex items-center pt-1 pb-0 font-semibold">
           my timetable
         </CardHeader>
         <CardContent className="w-full h-full flex flex-row overflow-hidden">
-          {showHours &&
-            <div id="timecol" className="w-1/8 h-full flex flex-col px-2 divide-y-[1px] divide-y-slate-50">
+          {showHours && (
+            <div
+              id="timecol"
+              className="w-1/8 h-full flex flex-col px-2 divide-y-[1px] divide-y-slate-50"
+            >
               <div className="h-14 w-full flex justify-center items-center">
                 Time
               </div>
-              <div id="hours-flex" className="h-full flex flex-col py-2 divide-y-[1px] divide-y-slate-50">
+              <div
+                id="hours-flex"
+                className="h-full flex flex-col py-2 divide-y-[1px] divide-y-slate-50"
+              >
                 {hours.map((e) => (
-                  <div key={e} className="w-full h-full flex justify-center items-start">
+                  <div
+                    key={e}
+                    className="w-full h-full flex justify-center items-start"
+                  >
                     {e}:00
                   </div>
                 ))}
               </div>
             </div>
-          }
-          <div id="cal" className={`px-4 w-full h-full grid ${gridLayouts[numberOfDays]} divide-x-[1px] divide-x-slate-50`}>
-            {displayDays.map((dayName: string, dayIdx: number) => (
-              DayComponent(
-                dayName,
-                dayIdx + startDay - 1,
-                courses,
-                { startHour, endHour, startDay, numberOfDays, showHours }
-              )
-            ))}
+          )}
+          <div
+            id="cal"
+            className={`px-4 w-full h-full grid ${gridLayouts[numberOfDays]} divide-x-[1px] divide-x-slate-50`}
+          >
+            {displayDays.map((dayName: string, dayIdx: number) =>
+              DayComponent(dayName, dayIdx + startDay - 1, courses, {
+                startHour,
+                endHour,
+                startDay,
+                numberOfDays,
+                showHours,
+              }),
+            )}
           </div>
         </CardContent>
-      </Card >
+      </Card>
     </div>
   );
 };
--- src/components/GeneralConfig.tsx
+++ src/components/GeneralConfig.tsx
@@ -1,8 +1,19 @@
 import type { CalendarConfig, Palette } from "@/lib/Types";
-import { CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Checkbox } from "./ui/checkbox";
-import paletteData from '@/assets/palettes.json';
+import paletteData from "@/assets/palettes.json";
 
 interface GeneralConfigProps {
   onConfigChange?: (config: CalendarConfig) => void;
@@ -11,24 +22,30 @@ interface GeneralConfigProps {
   setPalette: (palette: Palette) => void;
 }
 
-const GeneralConfig = ({ onConfigChange, config, palette, setPalette }: GeneralConfigProps) => {
-
-  const dayToInt: { [key: string]: number } = { // ew
-    "Monday": 1,
-    "Tuesday": 2,
-    "Wednesday": 3,
-    "Thursday": 4,
-    "Friday": 5,
-    "Saturday": 6,
-    "Sunday": 7
-  }
+const GeneralConfig = ({
+  onConfigChange,
+  config,
+  palette,
+  setPalette,
+}: GeneralConfigProps) => {
+  const dayToInt: { [key: string]: number } = {
+    // ew
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6,
+    Sunday: 7,
+  };
 
   return (
     <>
       <CardHeader className="pt-2">
         <CardTitle>General</CardTitle>
         <CardDescription>
-          Customize the look and feel of your timetable. All times are specified as 24-hour time.
+          Customize the look and feel of your timetable. All times are specified
+          as 24-hour time.
         </CardDescription>
       </CardHeader>
       <CardContent className="grid grid-cols-2 flex-col gap-2">
@@ -37,20 +54,23 @@ const GeneralConfig = ({ onConfigChange, config, palette, setPalette }: GeneralC
           <Select
             defaultOpen={false}
             defaultValue="Monday"
-            onValueChange={(value) => onConfigChange && onConfigChange({
-              ...config,
-              startDay: dayToInt[value]
-            })}
+            onValueChange={(value) =>
+              onConfigChange &&
+              onConfigChange({
+                ...config,
+                startDay: dayToInt[value],
+              })
+            }
           >
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Monday" />
             </SelectTrigger>
             <SelectContent>
-              {
-                Object.keys(dayToInt).map((day) => (
-                  <SelectItem key={day} value={day}>{day}</SelectItem>
-                ))
-              }
+              {Object.keys(dayToInt).map((day) => (
+                <SelectItem key={day} value={day}>
+                  {day}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
         </div>
@@ -59,18 +79,25 @@ const GeneralConfig = ({ onConfigChange, config, palette, setPalette }: GeneralC
           <Select
             defaultOpen={false}
             defaultValue={`${config.numberOfDays}`}
-            onValueChange={(value) => onConfigChange && onConfigChange({
-              ...config,
-              numberOfDays: parseInt(value)
-            })}
+            onValueChange={(value) =>
+              onConfigChange &&
+              onConfigChange({
+                ...config,
+                numberOfDays: parseInt(value),
+              })
+            }
           >
             <SelectTrigger className="w-1/4 min-w-14">
               <SelectValue placeholder={config.numberOfDays} />
             </SelectTrigger>
             <SelectContent>
-              {["1", "3", "5", "7"].map((dayIdx) => (
-                <SelectItem key={dayIdx} value={dayIdx}>{dayIdx}</SelectItem>
-              ))}
+              {[...Array(7).keys()]
+                .map((x) => (x + 1).toString())
+                .map((dayIdx) => (
+                  <SelectItem key={dayIdx} value={dayIdx}>
+                    {dayIdx}
+                  </SelectItem>
+                ))}
             </SelectContent>
           </Select>
         </div>
@@ -82,10 +109,14 @@ const GeneralConfig = ({ onConfigChange, config, palette, setPalette }: GeneralC
             max="24"
             className="w-full rounded border border-slate-200 p-2"
             value={config.startHour}
-            onChange={(e) => onConfigChange && onConfigChange({
-              ...config,
-              startHour: parseInt(e.target.value)
-            })} />
+            onChange={(e) =>
+              onConfigChange &&
+              onConfigChange({
+                ...config,
+                startHour: parseInt(e.target.value),
+              })
+            }
+          />
         </div>
         <div className="flex justify-start h-full items-center">End Time</div>
         <div className="flex justify-end">
@@ -95,42 +126,54 @@ const GeneralConfig = ({ onConfigChange, config, palette, setPalette }: GeneralC
             max="24"
             className="w-full rounded border border-slate-200 p-2"
             value={config.endHour}
-            onChange={(e) => onConfigChange && onConfigChange({
-              ...config,
-              endHour: parseInt(e.target.value)
-            })} />
+            onChange={(e) =>
+              onConfigChange &&
+              onConfigChange({
+                ...config,
+                endHour: parseInt(e.target.value),
+              })
+            }
+          />
         </div>
         <div className="flex justify-start h-full items-center">Show Hours</div>
         <div className="w-full flex justify-end p-2">
           <Checkbox
             checked={config.showHours}
-            onCheckedChange={(value) => onConfigChange && onConfigChange({
-              ...config,
-              showHours: value ? true : false
-            })} />
+            onCheckedChange={(value) =>
+              onConfigChange &&
+              onConfigChange({
+                ...config,
+                showHours: value ? true : false,
+              })
+            }
+          />
         </div>
-        <div className="flex justify-start h-full items-center">Calendar Palette</div>
+        <div className="flex justify-start h-full items-center">
+          Calendar Palette
+        </div>
         <div className="w-full flex justify-end p-2">
           <Select
             defaultOpen={false}
             defaultValue={palette.name}
-            onValueChange={(value) => setPalette(paletteData.find((p) => p.name === value) || palette)}
+            onValueChange={(value) =>
+              setPalette(paletteData.find((p) => p.name === value) || palette)
+            }
           >
             <SelectTrigger className="w-full">
               <SelectValue placeholder={palette.name} />
             </SelectTrigger>
             <SelectContent>
-              {
-                paletteData.map((palette, idx) => (
-                  <SelectItem key={palette.name} value={palette.name}>{palette.name}</SelectItem>
-                ))
-              }
+              {paletteData.map((palette, idx) => (
+                <SelectItem key={palette.name} value={palette.name}>
+                  {palette.name}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
         </div>
       </CardContent>
     </>
-  )
-}
+  );
+};
 
 export default GeneralConfig;
